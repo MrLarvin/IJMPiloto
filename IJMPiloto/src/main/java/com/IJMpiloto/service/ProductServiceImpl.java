2463b6c634514fb69e59e31ffed9a293da819305
alejandrofng <alejandrofng@gmail.com>
2016-03-22 15:08:32 -0430

    Changes made in ProductServiceImpl

--- IJMPiloto/src/main/java/com/IJMpiloto/service/ProductServiceImpl.java
+++ IJMPiloto/src/main/java/com/IJMpiloto/service/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.IJMpiloto.service;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.IJMpiloto.dao.ProductDao;
-import com.IJMpiloto.dao.SupplierDaoImpl;
 import com.IJMpiloto.dto.ProductDto;
+import com.IJMpiloto.dto.SupplierDto;
+
 import com.IJMpiloto.model.Product;
 import com.IJMpiloto.model.Supplier;
 
@@ -18,58 +20,68 @@ public class ProductServiceImpl implements ProductService {
 	
 	@Autowired
 	private ProductDao productDAO;
-	
 	@Autowired
-	private SupplierDaoImpl supplierDao;
+	private SupplierService supplierService;
 
 	@Override
-	public void saveProduct(ProductDto productDTO) {
+	public void saveProduct(ProductDto productDto) {
 		//codigo de mapeo
-		
 		Product product = new Product();
-		product.setCode(productDTO.getCode());
-		product.setDescription(productDTO.getDescription());
-		
-		
-		
+		product.setCode(productDto.getCode());
+		product.setDescription(productDto.getDescription());
+		Supplier supplier = supplierService.DtoToEntity(productDto.getSupplierDto());
+		product.setSupplier(supplier);
 		productDAO.save(product);
 	}
 
 	@Override
-	public void updateProduct(ProductDto productDto,long id) {
-		Product product = productDAO.findById(id);
+	public void updateProduct(ProductDto productDto, String code) {
+		Product product = productDAO.findByCode(code);
 		if (product != null) {
 			product.setCode(productDto.getCode());
 			product.setDescription(productDto.getDescription());
-			Supplier supplier = instanceSupplier(productDto.getSupplier_id());
+			Supplier supplier = supplierService.DtoToEntity(productDto.getSupplierDto());
 			product.setSupplier(supplier);
 		}
+		productDAO.update(product);
 	}
 
 	@Override
-	public void deleteProduct(Product product) {
+	public void deleteProduct(String code) {
+		Product product = productDAO.findByCode(code);
 		productDAO.delete(product);
 	}
 
 	@Override
-	public Product findProductById(long id) {
-		return productDAO.findById(id);
+	public ProductDto findProductDtoByCode(String code) {
+		Product product = productDAO.findByCode(code);
+		//mapping entity to Dto
+		ProductDto productDto = new ProductDto();
+		productDto.setCode(product.getCode());
+		productDto.setDescription(product.getDescription());
+		SupplierDto supplierDto = supplierService.EntityToDto(product.getSupplier());
+		productDto.setSupplierDto(supplierDto);
+		return productDto;
 	}
 
 	@Override
-	public List<Product> findAllProducts() {
-		return productDAO.findAll();
+	public List<ProductDto> findAllProductsDto() {
+		List<Product> products = productDAO.findAll();
+		List<ProductDto> productsDto = new LinkedList<>();
+		for (Product product : products) {
+			ProductDto productDto = new ProductDto();
+			productDto.setCode(product.getCode());
+			productDto.setDescription(product.getDescription());
+			SupplierDto supplierDto = supplierService.EntityToDto(product.getSupplier());
+			productDto.setSupplierDto(supplierDto);
+			productsDto.add(productDto);
+		}
+		return productsDto;
 	}
 
 	@Override
-	public boolean isProductExist(ProductDto product) {
-		return (productDAO.findByCode(product.getCode()).isEmpty());
-	}
-
-	@Override
-	public Supplier instanceSupplier(long id) {
-		
-		return supplierDao.findById(id);
+	public boolean isProductExist(String code) {
+		return (productDAO.findByCode(code)!=null);
 	}
 
 }
